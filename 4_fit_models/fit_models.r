81c3c88d7a07fdf9c6032d39165e4bf68b95e808
Bret Beheim <beheim@gmail.com>
2022-06-22 08:32:43 +0200

    misc

--- 4_fit_models/fit_models.r
+++ 4_fit_models/fit_models.r
@@ -31,23 +31,6 @@ dat_list <- list(
   N_b_age_group = length(unique(d$b_age_group))
 )
 
-inspect_data_list <- function(dl) {
-  dl_lengths <- lapply(dl, length)
-  lengths <- sort(unique(unlist(dl_lengths)))
-  check_lengths <- lengths[lengths > 1]
-  for (i in 1:length(check_lengths)) {
-    tar <- which(dl_lengths == check_lengths[i])
-    sub_df <- as.data.frame(dl[tar])
-    check_cor <- cor(sub_df)
-    no_lindep <- !any(check_cor[lower.tri(check_cor)] > 0.9)
-    stopifnot(no_lindep)
-  }
-  return(dl)
-}
-
-# yeesh...no wonder it wont fit, the calcs are wrong
-plot(d$pop_44, d$pop_44xpop_win_44)
-
 
 horizon24 <- stan(file = "./stan/horizon24.stan", data = dat_list,
   iter = n_iter, chains = 3, cores = 3)
