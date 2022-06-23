bd84f8c5e89cdcd65713b187537852a3a37fc0b7
Bret Beheim <beheim@gmail.com>
2022-06-23 22:12:46 +0200

    misc tweaks

--- project_support.R
+++ project_support.R
@@ -3,13 +3,11 @@ library(rethinking)   # needed for model exploration
 library(rstan)
 library(RColorBrewer) # needed for figures
 library(tictoc)
-library(knitr)
-library(testthat)
 library(kaya) # github.com/babeheim/kaya
 library(digest)
 
-expect_true(capabilities("png"))
-expect_true(capabilities("cairo"))
+stopifnot(capabilities("png"))
+stopifnot(capabilities("cairo"))
 
 scaffold <- TRUE
 n_iter <- 2000
